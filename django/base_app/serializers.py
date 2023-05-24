84c1dd4b84ee8eb89a97efd005f8683674e004be
Richard Ding <rqding@gmail.com>
2023-05-24 15:41:37 +0100

    Update the API of file serializers for file creation

--- django/base_app/serializers.py
+++ django/base_app/serializers.py
@@ -1,4 +1,12 @@
 from rest_framework import serializers
+from .models import UploadedFile
 
-class FileSerializer(serializers.Serializer):
-    file = serializers.FileField()
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UploadedFile
+        fields = '__all__'
+
+    def create(self, validated_data):
+        file = validated_data['file']  # Assuming the field name for the file is 'file'
+        uploaded_file = UploadedFile.objects.create(file=file)
+        return uploaded_file
